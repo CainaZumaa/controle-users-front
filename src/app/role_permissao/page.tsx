1be9f91258bc4bd41393d38b3c8701285142142d
Calebe-Moura <mouracalebe@hotmail.com>
2025-07-14 22:07:31 -0300

    create page, role_permissao,DataGridDemo,rolespermissoService

--- src/app/role_permissao/page.tsx
+++ src/app/role_permissao/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React from "react";
-import UsersDataGrid from "../../components/UsersDataGrid";
+import DataGridDemo from "../../components/DataGridDemo";
 
 export default function UserPage() {
   return (
     <div className="w-full h-full px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
-        <UsersDataGrid />
+        <DataGridDemo />
       </div>
     </div>
   );
